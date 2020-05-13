a9a6fe479361844b57fe662768a6489353ac86b8
BenedictWilkinsAI <benrjw@gmail.com>
2020-05-13 10:38:52 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 01 12:42:18 2020
+
+@author: Benedict Wilkins
+"""
+
+from setuptools import setup
+
+setup(name='datasets',
+      version='0.0.1',
+      description='A collection of datasets',
+      url='',
+      author='Benedict Wilkins',
+      author_email='brjw@hotmail.co.uk',
+      license='MIT',
+      packages=['datasets'],
+      zip_safe=False)
